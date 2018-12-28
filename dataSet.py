aa0fd122f69081d9a947862a9802e681a3a1298f
awronka <alexiusmichael@gmail.com>
2018-12-27 21:17:49 -0500

    add start stuff

--- dataSet.py
+++ dataSet.py
@@ -21,3 +21,25 @@ VVIX=Quandl("CBOE/VVIX",start_date=data_Start_REG,end_date=data_END_REG,order="a
 LSGLX=Quandl("YAHOO/FUND_LSGLX",start_date=data_Start_REG,end_date=data_END_REG,order="asc",type="xts")[,4]
 PRITX=Quandl("YAHOO/FUND_PRTIX",start_date=data_Start_REG,end_date=data_END_REG,order="asc",type="xts")[,4]
 INDEXX=index(FFM)
+
+
+getSymbols(c("^GSPC","^VIX","^VXV","XLB", "XLE", "XLF", "XLI", "XLK", "XLP", "XLU", "XLV", "XLY", "RWR", "SHY"),from = as.Date("2007-11-04"), to = as.Date("2018-04-04"))
+
+https://rdrr.io/rforge/qmao/man/getSymbols.cfe.html
+https://stackoverflow.com/questions/13282094/download-vix-futures-prices-from-cboe,
+https://bommaritollc.com/2012/11/05/retrieving-the-vix-term-structure-in-r/
+
+https://rpubs.com/JanpuHou/378663
+https://r-forge.r-project.org/scm/viewvc.php/pkg/qmao/R/getSymbols.cfe.R?r1=352&r2=351&sortby=author&root=twsinstrument&pathrev=352&diff_format=c
+https://r-forge.r-project.org/scm/viewvc.php/*checkout*/pkg/qmao/R/getSymbols.cfe.R?revision=430&root=twsinstrument
+https://quantstrattrader.wordpress.com/2017/05/18/constant-expiry-vix-futures-using-public-data/
+
+http://systematicinvestor.github.io/Volatility-Strategy
+https://github.com/gsee/qmao/blob/master/R/getSymbols.cfe.R
+http://www.ntu.edu.sg/home/nprivault/MA5182/volatility-estimation.pdf
+
+##SKEW...VXTH...VIX###
+### "GOOG/AMEX_SHY","KFRENCH/FACTORS_D", "YAHOO/INDEX_IRX", "CBOE/SKEW", "CBOE/VIX", "CBOE/VXTH" #####
+
+https://www.quandl.com/data/URC-Unicorn-Research-Corporation
+http://www.cboe.com/products/vix-index-volatility/vix-related-strategy-benchmarks/cboe-low-volatility-index-lovol
--- main.py
+++ main.py
@@ -1,20 +1,39 @@
-from flask import Flask
+from flask import Flask, request
 import quandl
 import pandas as pd
 import numpy as np
 from sklearn.datasets import make_regression
 from sklearn.linear_model import Lasso
 import os
+import requests
+from urllib.request import urlopen
+from datetime import datetime
+import pandas_datareader.data as web
+# import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt;
 
 app = Flask(__name__)
+quandl.ApiConfig.api_key = os.getenv('QUANDLE_API_KEY')
+AV_API_KEY = os.getenv('ALPHA_VANTAGE')
+ALPHA_VANTAGE_URL = os.getenv('ALPHA_VANTAGE_URL')
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/getVix')
+def getVix():
+    start = datetime(2010,1,1)
+    end = datetime(2014,3,24)
+    ticker = "AAPL"
+    f= web.DataReader(['CPIAUCSL', 'CPILFESL'], 'fred', start, end)
+    # plt.plot(f['Close'])
+    # plt.title('AAPL Closing Prices')
+    # plt.show()
+    return str(f)
+
 @app.route('/get_stock')
 def get_stock():
-    quandl.ApiConfig.api_key = os.getenv('QUANDLE_API_KEY')
     test = quandl.get('NASDAQOMX/XQC', start_date='2018-12-17', end_date='2018-12-17')
     return test.to_json()
 
@@ -45,5 +64,40 @@ def get_lasso():
 
     return str(np.mean((clf.coef_ != 0.0) == (coef != 0.0)))
 
+@app.route('/get_test')
+def get_test():
+    return quandl.get('ML/EEMCBI', start_date='2008-04-01', end_date='2028-10-01').to_json()
 
+
+@app.route('/stock_data')
+def stock_data():
+    ticker = request.args['ticker']
+    url = f'{ALPHA_VANTAGE_URL}/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={AV_API_KEY}'
+    r = requests.get(url)
+    return str(r.json())
+
+@app.route('/cboe')
+def getPutCallRatio():
+    """ download current Put/Call ratio"""
+    urlStr = 'http://www.cboe.com/publish/ScheduledTask/MktData/datahouse/totalpc.csv'
+
+    try:
+        lines = urlopen(urlStr).readlines()
+    except Exception as e:
+        s = "Failed to download:\n{0}".format(e)
+        print (s)
+       
+    headerLine = 2
+
+    header = lines[headerLine].decode("utf-8").strip().split(',')
     
+    data =   [[] for i in range(len(header))]
+    
+    for line in lines[(headerLine+1):]:
+        fields = line.rstrip().decode("utf-8").split(',')
+        print(fields)
+        data[0].append(datetime.strptime(fields[0],'%m/%d/%Y'))
+        for i,field  in enumerate(fields[1:]):
+            data[i+1].append(float(field))
+    
+    return pd.DataFrame(dict(zip(header[1:],data[1:])), index = pd.Index(data[0])).to_json()
